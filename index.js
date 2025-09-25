5dd1b9a52ae4adcfc318a4a645be9d3ba6b44ff8
JashMistry83 <jashmistry1721@gmail.com>
2025-09-25 22:16:10 +0530

    feat: initial PERN stack project commit

---
+++ index.js
@@ -0,0 +1,90 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "ToDoList",
+  password: "postgres123",
+  port: 5432,
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  try {
+    const items = await db.query("SELECT * FROM todolist");
+
+    console.log("/ GET REQ - ITEMS = ", items.rows);
+
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items.rows,
+    });
+  } catch (error) {
+    console.log("Error occurs...", error);
+  }
+});
+
+app.post("/add", async (req, res) => {
+  try {
+    const item = req.body.newItem;
+
+    console.log("/add POST REQ - INSERT - ITEM = ", item);
+
+    const insertItem = await db.query(
+      "INSERT INTO todolist(title) VALUES($1)",
+      [item]
+    );
+
+    res.redirect("/");
+  } catch (error) {
+    console.log("Error occurs...", error);
+  }
+});
+
+app.post("/edit", async (req, res) => {
+  try {
+    const item = req.body;
+
+    const titleUpdate = item.updatedItemTitle;
+    const idUpdate = item.updatedItemId;
+
+    console.log("/edit POST REQ - UPDATE - ITEM = ", item);
+
+    const updateItem = await db.query(
+      "UPDATE todolist SET title = ($1) WHERE id = ($2)",
+      [titleUpdate, idUpdate]
+    );
+
+    res.redirect("/");
+  } catch (error) {
+    console.log("Error occurs...", error);
+  }
+});
+
+app.post("/delete", async (req, res) => {
+  try {
+    const itemDelete = req.body.deleteItemId;
+
+    console.log("/delete DELETE REQ - DELETE - ITEM = ", itemDelete);
+
+    const deleteItem = await db.query("DELETE FROM todolist WHERE id = ($1)", [
+      itemDelete,
+    ]);
+
+    res.redirect("/");
+  } catch (error) {
+    console.log("Error occurs...", error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}/`);
+});
